505214e98e4c161a35079bf8ec9170fedfd5530b
ken <tingyuliu0223@gmail.com>
2024-06-10 13:49:36 +0800

    update run.sh

---
+++ result.cpp
@@ -0,0 +1,20 @@
+// 70
+// 60
+// 326
+// 13.046460
+#include <stdio.h>
+
+int main() {
+	int i = 5;
+	float j = 65.232300;
+	int resultAdd = 70;
+	int resultMin = 60;
+	int resultMul = 326;
+	float resultDiv = 13.046460;
+	printf("70");
+	printf("60\n");
+	printf("326\n");
+	printf("13.046460\n");
+
+	return 0;
+}
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-bison -d yacc.y
-flex lex.l
-g++ yacc.tab.c lex.yy.c -o qv_compiler -ll
+sudo make 
+./main > result.cpp
